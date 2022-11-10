e1a9f577242a632d6813110b5469067176591bf2
João Victor Nunes de Moura <jotaven.moura@gmail.com>
2022-11-10 20:25:41 -0300

    otimização das paginas

--- index.php
+++ index.php
@@ -24,9 +24,45 @@ if(isset($_GET['p'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/header_style.css" />
+	<script src="https://kit.fontawesome.com/682b28ed24.js" crossorigin="anonymous"></script>
+    <title>Loja - MangaXpress</title>
 </head>
 <body>
+    <header>
+        <nav class="principal-nav">
+            <div class="nav-container">
+                <a href="" class="brand">
+                    <h1>
+                        <span id="brand-name1">Manga</span
+                        ><span id="brand-name2">Xpress</span>
+                    </h1>
+                </a>
+                <div class="search">
+                    <input type="text" name="#" id="#" class="search-bar" />
+                    <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+                </div>
+                <div class="menu">
+                    <a href="#"><i class="fa-solid fa-user"></i></a>
+                    <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                    <a href="#"><i class="fa-solid fa-heart"></i></a>
+                    <a href="#"><i class="fa-solid fa-bookmark"></i></a>
+                </div>
+                <button class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+            </div>
+        </nav>
+        <nav class="secundary-nav">
+            <div class="nav-container-secundary">
+                <a href="">Destaques</a>
+                <a href="">Lançamentos</a>
+                <a href="">Pré-vendas</a>
+            </div>
+        </nav>
+	</header>
     <div class="main-body">
         <div class="page-wrapper">
             <?php include('pages/' . $pagina); ?>
--- inicial.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-include_once("layout/header.html");
-include_once("pages/index.html");
