ad68e4b3825ad4fefb3f3867ba8bae7bcfa90696
Lara Lopes Marques <102087173+larallm@users.noreply.github.com>
2022-11-16 19:42:21 -0300

    footer

--- index.php
+++ index.php
@@ -83,6 +83,35 @@ if (isset($_GET['p'])) {
             <?php include('pages/' . $pagina); ?>
         </div>
     </div>
+
+    <footer>
+	<nav class="footer-main">
+		<div class="footer-content">
+			<a href="" class="footer.names">
+				<h1>
+					<span id="footer-name1">Manga</span
+					><span id="footer-name2">Xpress</span>
+				</h1>
+			</a>
+			<p>Sua loja de compras online de mangas!</p>
+			<ul class="footer-social-media">
+				<li>
+					<a href="#"><i class="fa fa-facebook"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-instagram"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+				</li>
+			</ul>
+		</div>
+		<div class="footer-end">
+			<p>MangaXpress &copy;2022</p>
+		</div>
+	</nav>
+</footer>
+
 </body>
 
 </html>
