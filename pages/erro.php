2b1b0e993f2d7e8830b3a119c2aa7cb4cd266eb6
João Victor Nunes de Moura <94320199+jotaven@users.noreply.github.com>
2022-11-30 09:53:04 -0300

    Update erro.php

--- pages/erro.php
+++ pages/erro.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="assets/css/erro404.css">
 
-<img src="assets/img/backgrounds/zoro2.png" height="700vh">
+<img src="assets/img/backgrounds/zoro2.png" height="70%">
 <div id="container">
     <div class="content">
         <h2>404</h2>
